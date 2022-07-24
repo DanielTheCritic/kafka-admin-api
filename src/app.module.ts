3c0944abecd93a605b9572763f7db9ad2ea33ac3
Daniel Sansom <daniel.jon.sansom@gmail.com>
2022-07-24 23:00:57 +0200

    Added docker file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TopicsModule } from './topics/topics.module';
 
 @Module({
-  imports: [TopicsModule],
+  imports: [
+    TopicsModule,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
