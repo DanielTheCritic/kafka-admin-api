78c6bae125a386f3ce956de548db300ef1c6bfd7
Daniel Sansom <daniel.jon.sansom@gmail.com>
2022-08-02 21:29:04 +0200

    Fixed unit tests.

--- src/topics/topics.service.spec.ts
+++ src/topics/topics.service.spec.ts
@@ -1,14 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { KafkaIntegrationService } from '../kafka-integration/kafka-integration-service';
 import { TopicsService } from './topics.service';
+
+const mockKafkaIntegrationService = () => ({
+  getTopics: jest.fn(),
+  getOrCreateTopic: jest.fn(),
+});
 
 describe('TopicsService', () => {
   let service: TopicsService;
+  let kafkaIntegrationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TopicsService],
+      providers: [
+        TopicsService,
+        {
+          provide: KafkaIntegrationService,
+          useFactory: mockKafkaIntegrationService,
+        },
+      ],
     }).compile();
 
+    kafkaIntegrationService = module.get<KafkaIntegrationService>(
+      KafkaIntegrationService,
+    );
     service = module.get<TopicsService>(TopicsService);
   });
 
